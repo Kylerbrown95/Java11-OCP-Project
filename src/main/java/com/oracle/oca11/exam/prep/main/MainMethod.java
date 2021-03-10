0f5b2575fb0d911584e3f1cabc04a548de0f66b2
Kyle Brown <kyle.r.brown@hotmail.co.uk>
2021-03-10 16:47:17 +0000

    part 3

---
+++ src/main/java/com/oracle/oca11/exam/prep/main/MainMethod.java
@@ -0,0 +1,23 @@
+package com.oracle.oca11.exam.prep.main;
+
+public class MainMethod {
+	
+	public static void main(String args[]) {
+		// program execute starts here
+		
+		System.out.println("Hello World!");
+	}
+	
+	//Single comment
+	
+	/*
+	 * Multi
+	 * Line
+	 * Comments
+	 */
+	
+	/**
+	 * Javadoc
+	 */
+
+}
