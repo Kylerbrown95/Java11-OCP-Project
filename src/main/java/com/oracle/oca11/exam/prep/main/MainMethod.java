7c0ee8e0ae408e3a02a1337cb93710e2664a3b83
Kyle Brown <kyle.r.brown@hotmail.co.uk>
2021-03-10 16:50:35 +0000

    Correction

--- src/main/java/com/oracle/oca11/exam/prep/main/MainMethod.java
+++ src/main/java/com/oracle/oca11/exam/prep/main/MainMethod.java
@@ -2,7 +2,7 @@ package com.oracle.oca11.exam.prep.main;
 
 public class MainMethod {
 	
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		// program execute starts here
 		
 		System.out.println("Hello World!");
